343b378af3bfb9f3159fb14e8dc916f5ab63d1fe
Robeen M <maharjanrobeen@gmail.com>
2022-04-10 21:47:16 -0700

    Part one Completed. Added Note creation functionality with canceling into app.js and followed with stlying in style.css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,39 @@
-const notes = [
-  { 
-    title: "first note", 
+const notes = [{
+    title: "first note",
     noteBody: "this is an example note",
-    id: 1 
-  }
-]
+    id: 1
+}]
+
+const writingArea = document.querySelector(".write-note-area")
+
+
+const newNote = `
+<div class="creation">
+  <textarea placeholder="Type Something" rows='7' cols='85'> </textarea> 
+</div>
+<div class="buttons">
+  <button class = saveBTN>save</button>
+  <button class = cancelBTN>cancel</button>
+</div>
+`
+
+const plusButton = document.querySelector(".fa-solid.fa-circle-plus")
+plusButton.addEventListener('click', createNote)
+
+function createNote() {
+    if (canCreate()) {
+        writingArea.insertAdjacentHTML("beforeend", newNote)
+    }
+    const buttonClear = document.querySelector(".cancelBTN")
+    buttonClear.addEventListener('click', clearNotes)
+}
+
+function clearNotes() {
+    document.querySelector(".creation").remove()
+    document.querySelector(".buttons").remove()
+
+}
+
+function canCreate() {
+    return !(document.querySelector(".creation"))
+}
