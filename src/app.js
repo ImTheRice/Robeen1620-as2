dfb4550e896ac77e8077b3e0b96e7ff1fc7fc5ac
Robeen M <maharjanrobeen@gmail.com>
2022-04-11 00:28:24 -0700

    Part Two completed. Save a note functionality added to app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const writingArea = document.querySelector(".write-note-area")
 
 const newNote = `
 <div class="creation">
-  <textarea placeholder="Type Something" rows='7' cols='85'> </textarea> 
+  <textarea placeholder="Type Something" rows='7' cols='85'></textarea> 
 </div>
 <div class="buttons">
   <button class = saveBTN>save</button>
@@ -20,20 +20,44 @@ const newNote = `
 const plusButton = document.querySelector(".fa-solid.fa-circle-plus")
 plusButton.addEventListener('click', createNote)
 
+function canCreate() {
+    return !(document.querySelector(".creation"))
+}
+
 function createNote() {
     if (canCreate()) {
         writingArea.insertAdjacentHTML("beforeend", newNote)
     }
     const buttonClear = document.querySelector(".cancelBTN")
     buttonClear.addEventListener('click', clearNotes)
+
+    const buttonSave = document.querySelector(".saveBTN")
+    buttonSave.addEventListener('click', saveNotes)
 }
 
 function clearNotes() {
     document.querySelector(".creation").remove()
     document.querySelector(".buttons").remove()
-
 }
 
-function canCreate() {
-    return !(document.querySelector(".creation"))
+function saveNotes() {
+    const fullNote = document.querySelector(".creation textarea").value
+
+    if (fullNote == '') {
+        return
+    }
+
+    const words = fullNote.split('\n')
+    const titleName = (words[0])
+    const content = words.splice(1).join('\n')
+
+    const notePackage = {
+        title: titleName,
+        noteBody: content,
+        id: notes.length + 1
+    }
+
+    notes.push(notePackage)
+
+    clearNotes()
 }
